1aee5c48e4b696293b31a9699de8431041916c0a
Andrew Smith <andrew@andrewnsmith.net>
2022-05-06 23:31:28 -0400

    Setup working bot boilerplate

--- bot.py
+++ bot.py
@@ -5,10 +5,12 @@ import discord.ext.commands
 class Bot(discord.ext.commands.Bot):
     def __init__(self):
         super().__init__(command_prefix='$')
-    @super().command
-    async def test(ctx, arg1):
-        await ctx.send(f'You passed {arg1}')
+
+        @self.command()
+        async def test(ctx, arg1):
+            print(f"Arg1: {arg1}")
+            await ctx.send(f"Arg1: {arg1}")
 
 
 if __name__ == "__main__":
-    bot = Bot().run()
+    Bot().run("***REMOVED***")
