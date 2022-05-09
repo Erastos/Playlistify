7ec2449bf0af30bc359b3dd2bef27e334eef2a75
Andrew Smith <andrew@andrewnsmith.net>
2022-05-09 11:22:41 -0400

    Add needed scopes

--- spotify.py
+++ spotify.py
@@ -4,7 +4,8 @@ from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 load_dotenv()
 
+SCOPES = ["playlist-modify-public", "playlist-modify-private"]
+
 class SpotifyClient():
     def __init__(self, scope):
-        self.client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-
+        self.client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPES))
