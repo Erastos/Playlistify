d2a0c1ec0c4b5f214f8aac803418df030340444c
Andrew Smith <andrew@andrewnsmith.net>
2022-05-09 23:52:31 -0400

    Seperate out spotify authentication logic

--- spotify.py
+++ spotify.py
@@ -8,8 +8,21 @@ SCOPES = ["playlist-modify-public", "playlist-modify-private"]
 
 class SpotifyClient():
     def __init__(self):
-        self.client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=SCOPES))
+        self.client = None
+        self.auth = SpotifyOAuth(scope=SCOPES, open_browser=False)
+        self.authorize_url = self.auth.get_authorize_url()
+        self.auth_object = None
+
+    def authorizeClient(self, response_url):
+        response_code = self.auth.parse_response_code(response_url)
+        self.auth_object = self.auth.get_access_token(response_code)
+        self.access_token = self.auth_object["access_token"]
+        self.client = spotipy.Spotify(auth=self.access_token)
 
 if __name__ == "__main__":
     client = SpotifyClient()
+    print(f"Authorize URL: {client.authorize_url}")
+    response_url = input("Response URL: ")
+    client.authorizeClient(response_url)
+
     print(client.client.me())
